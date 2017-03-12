03f9bf9692c8b6479fda98fe743e0d195511c265
Fingercomp <fingercomp@users.noreply.github.com>
2017-03-12 23:45:39 +0700

    Update surface updater

--- src/glasses/surface.lua
+++ src/glasses/surface.lua
@@ -5,7 +5,6 @@ local module = require("ut2-serv.modules")
 local config = module.load("config")
 local db = module.load("db")
 local events = module.load("events")
-local drawUI = module.load("glasses.ui")
 
 local glasses = com.glasses
 
@@ -18,39 +17,41 @@ local function convColor(color)
   return r / 0xff, g / 0xff, b / 0xff
 end
 
+local objects = {}
+
 local function addObject(...)
   local args = {...}
   if args[2] == "addBox" then
     local rect = glasses.addRect()
     rect.setPosition(args[3], args[4])
-    rect.setSize(args[5], args[6])
+    rect.setSize(args[6], args[5])
     rect.setColor(convColor(args[7]))
     if args[8] then
       rect.setAlpha(args[8])
     end
+    objects[args[1]] = rect
     return rect
   elseif args[2] == "addText" then
-    local text = glasses.addText()
+    local text = glasses.addTextLabel()
     text.setPosition(args[3], args[4])
     text.setText(args[5])
-    text.setText(args[6])
+    text.setColor(convColor(args[6]))
     text.setScale(1)
+    objects[args[1]] = text
     return text
   elseif args[2] == "addLine" then
     local quad = glasses.addQuad()
     quad.setVertex(1, args[3].x, args[3].y)
     quad.setVertex(2, args[4].x, args[4].y)
     quad.setColor(convColor(args[5]))
+    objects[args[1]] = quad
     return quad
   elseif args[2] == "addPolygon" then
     local quad = glasses.addQuad()
     quad.setColor(convColor(args[3]))
     quad.setAlpha(args[4])
     for i = 1, #args - 4, 1 do
-      quad.setVertex(i, args[i].x, args[i].y)
-    end
-    for i = #args - 4 + 1, 4, 1 do
-      quad.setVertex(i, args[#args - 4].x, args[#args - 4].y)
+      quad.setVertex(i, args[i + 4].x, args[i + 4].y)
     end
     if #args > 8 then
       local quad2 = glasses.addQuad()
@@ -70,20 +71,21 @@ end
 
 local function drawUI()
   -- SCORE
+  addObject("score.green.box", "addBox", 55, 5, 50, 50, 0x20ff20, .33)
   addObject("score.blue.box", "addBox", 5, 5, 50, 50, 0x20afff, .33)
-  local score1 = addObject("score.blue.text", "addText", 13, 20, "00", 0xffffff)
+  addObject("score.red.box", "addBox", 105, 5, 50, 50, 0xff2020, .33)
+  addObject("score.yellow.box", "addBox", 155, 5, 50, 50, 0xffff20, .33)
+
+  local score1 = addObject("score.blue.text", "addText", 13 / 3, 20 / 3, "00", 0xffffff)
   score1.setScale(3)
 
-  addObject("score.green.box", "addBox", 55, 5, 50, 50, 0x20ff20, .33)
-  local score2 = addObject("score.green.text", "addText", 63, 20, "09", 0xffffff)
+  local score2 = addObject("score.green.text", "addText", 63 / 3, 20 / 3, "00", 0xffffff)
   score2.setScale(3)
 
-  addObject("score.red.box", "addBox", 105, 5, 50, 50, 0xff2020, .33)
-  local score3 = addObject("score.red.text", "addText", 113, 20, "18", 0xffffff)
+  local score3 = addObject("score.red.text", "addText", 113 / 3, 20 / 3, "00", 0xffffff)
   score3.setScale(3)
 
-  addObject("score.yellow.box", "addBox", 155, 5, 50, 50, 0xffff20, .33)
-  local score4 = addObject("score.yellow.text", "addText", 163, 20, "27", 0xffffff)
+  local score4 = addObject("score.yellow.text", "addText", 163 / 3, 20 / 3, "00", 0xffffff)
   score4.setScale(3)
 
   -- SCORE PRBAR
@@ -97,176 +99,53 @@ local function drawUI()
 
   addObject("time.separator", "addLine", {x=105, y=63}, {x=105, y=107}, 0x606060)
 
-  local passed = addObject("time.passed.time", "addText", 18, 75, "01:23", 0x000000)
+  local passed = addObject("time.passed.time", "addText", 18 / 3, 75 / 3, "00:00", 0x000000)
   passed.setScale(3)
   addObject("time.passed.label", "addText", 18, 65, "Time passed", 0x000000)
 
-  local total = addObject("time.total.time", "addText", 118, 70, "05:00", 0x000000)
+  local total = addObject("time.total.time", "addText", 118 / 1.75, 70 / 1.75, "00:00", 0x000000)
   total.setScale(1.75)
-  local totalLabel = addObject("time.total.label", "addText", 118, 63, "Total", 0x000000)
+  local totalLabel = addObject("time.total.label", "addText", 118 / 0.8, 63 / 0.8, "Total", 0x000000)
   totalLabel.setScale(0.8)
 
-  local remaining = addObject("time.remaining.time", "addText", 118, 95, "03:37", 0x000000)
+  local remaining = addObject("time.remaining.time", "addText", 118 / 1.75, 95 / 1.75, "00:00", 0x000000)
   remaining.setScale(1.75)
-  local remainingLabel = addObject("time.remaining.label", "addText", 118, 88, "Remaining", 0x000000)
+  local remainingLabel = addObject("time.remaining.label", "addText", 118 / 0.8, 88 / 0.8, "Remaining", 0x000000)
   remainingLabel.setScale(0.8)
 
   -- TIME PRBAR
   addObject("time.bar.total", "addBox", 5, 112, 200, 1, 0xffffff)
-  addObject("time.bar.passed", "addBox", 5, 112, 55, 1, 0x20afff)
+  addObject("time.bar.passed", "addBox", 5, 112, 0, 1, 0x20afff)
 
   -- NICKS
   addObject("nicks.blue.box.small", "addBox", 210, 5, 5, 11, 0x20afff)
   addObject("nicks.blue.box", "addBox", 215, 5, 150, 11, 0x20afff, .5)
-  addObject("nicks.blue.text", "addText", 217, 7, "Fingercomp", 0xffffff)
 
   addObject("nicks.green.box.small", "addBox", 210, 18, 5, 11, 0x20ff20)
   addObject("nicks.green.box", "addBox", 215, 18, 150, 11, 0x20ff20, .5)
-  addObject("nicks.green.text", "addText", 217, 20, "Fingercomp", 0xffffff)
 
   addObject("nicks.red.box.small", "addBox", 210, 31, 5, 11, 0xff2020)
   addObject("nicks.red.box", "addBox", 215, 31, 150, 11, 0xff2020, .5)
-  addObject("nicks.red.text", "addText", 217, 33, "Fingercomp", 0xffffff)
 
   addObject("nicks.yellow.box.small", "addBox", 210, 44, 5, 11, 0xffff20)
   addObject("nicks.yellow.box", "addBox", 215, 44, 150, 11, 0xffff20, .5)
-  addObject("nicks.yellow.text", "addText", 217, 46, "Fingercomp", 0xffffff)
 
-  -- LOGO
-  local rs = 5
-  local rx = 20
-  local ry = -22.5
-  local robotTop1 = addObject("logo.robot.top.1", "addPolygon", 0x000000, .5,
-    {x = 0 * rs + rx, y = -5 * rs + ry},
-    {x = 2 * rs + rx, y = -6 * rs + ry},
-    {x = 2 * rs + rx, y = -5 * rs + ry})
-  local robotTop2 = addObject("logo.robot.top.2", "addPolygon", 0x000000, .5,
-    {x = 3 * rs + rx, y = -9 * rs + ry},
-    {x = 3.5 * rs + rx, y = -7 * rs + ry},
-    {x = 3 * rs + rx, y = -6 * rs + ry},
-    {x = 2 * rs + rx, y = -5 * rs + ry},
-    {x = 2 * rs + rx, y = -6 * rs + ry})
-  local robotTop3 = addObject("logo.robot.top.3", "addPolygon", 0x000000, .5,
-    {x = 2 * rs + rx, y = -5 * rs + ry},
-    {x = 3 * rs + rx, y = -6 * rs + ry},
-    {x = 5 * rs + rx, y = -6 * rs + ry},
-    {x = 6 * rs + rx, y = -5 * rs + ry})
-  local robotTop4 = addObject("logo.robot.top.4", "addPolygon", 0x000000, .5,
-    {x = 5 * rs + rx, y = -9 * rs + ry},
-    {x = 4.5 * rs + rx, y = -7 * rs + ry},
-    {x = 5 * rs + rx, y = -6 * rs + ry},
-    {x = 6 * rs + rx, y = -5 * rs + ry},
-    {x = 6 * rs + rx, y = -6 * rs + ry})
-  local robotTop5 = addObject("logo.robot.top.5", "addPolygon", 0x000000, .5,
-    {x = 8 * rs + rx, y = -5 * rs + ry},
-    {x = 6 * rs + rx, y = -6 * rs + ry},
-    {x = 6 * rs + rx, y = -5 * rs + ry})
-
-  local robotBot1 = addObject("logo.robot.bottom.1", "addPolygon", 0x000000, .5,
-    {x = 0 * rs + rx, y = -4 * rs + ry},
-    {x = 2 * rs + rx, y = -2 * rs + ry},
-    {x = 2 * rs + rx, y = -3.5 * rs + ry},
-    {x = 1.5 * rs + rx, y = -4 * rs + ry})
-  local robotBot2 = addObject("logo.robot.bottom.2", "addPolygon", 0x000000, .5,
-    {x = 2 * rs + rx, y = -2 * rs + ry},
-    {x = 2 * rs + rx, y = -3.5 * rs + ry},
-    {x = 2.5 * rs + rx, y = -4 * rs + ry},
-    {x = 5.5 * rs + rx, y = -4 * rs + ry},
-    {x = 6 * rs + rx, y = -3.5 * rs + ry},
-    {x = 6 * rs + rx, y = -2 * rs + ry},
-    {x = 4 * rs + rx, y = 0 * rs + ry})
-  local robotBot3 = addObject("logo.robot.bottom.3", "addPolygon", 0x000000, .5,
-    {x = 8 * rs + rx, y = -4 * rs + ry},
-    {x = 6 * rs + rx, y = -2 * rs + ry},
-    {x = 6 * rs + rx, y = -3.5 * rs + ry},
-    {x = 6.5 * rs + rx, y = -4 * rs + ry})
-
-  local robotMid1 = addObject("logo.robot.middle.1", "addPolygon", 0xffffff, .5,
-    {x = 1.5 * rs + rx, y = -5 * rs + ry},
-    {x = 1.5 * rs + rx, y = -4 * rs + ry},
-    {x = 2 * rs + rx, y = -3.5 * rs + ry},
-    {x = 2.5 * rs + rx, y = -4 * rs + ry},
-    {x = 2.5 * rs + rx, y = -5 * rs + ry})
-  local robotMid2 = addObject("logo.robot.middle.2", "addPolygon", 0xff2020, .5,
-    {x = 2.5 * rs + rx, y = -5 * rs + ry},
-    {x = 2.5 * rs + rx, y = -4 * rs + ry},
-    {x = 5.5 * rs + rx, y = -4 * rs + ry},
-    {x = 5.5 * rs + rx, y = -5 * rs + ry})
-  local robotMid3 = addObject("logo.robot.middle.3", "addPolygon", 0xffffff, .5,
-    {x = 5.5 * rs + rx, y = -5 * rs + ry},
-    {x = 5.5 * rs + rx, y = -4 * rs + ry},
-    {x = 6 * rs + rx, y = -3.5 * rs + ry},
-    {x = 6.5 * rs + rx, y = -4 * rs + ry},
-    {x = 6.5 * rs + rx, y = -5 * rs + ry})
-
-  local logoRed1 = addObject("logo.logo.red.1", "addPolygon", 0xff2020, .5,
-    {x = 3 * rs + rx, y = -9 * rs + ry},
-    {x = 3 * rs + rx, y = -12.5 * rs + ry},
-    {x = 5 * rs + rx, y = -12.5 * rs + ry},
-    {x = 5 * rs + rx, y = -9 * rs + ry},
-    {x = 4.5 * rs + rx, y = -7 * rs + ry},
-    {x = 3.5 * rs + rx, y = -7 * rs + ry})
-  local logoRed2 = addObject("logo.logo.red.2", "addPolygon", 0xff2020, .5,
-    {x = 2.5 * rs + rx, y = -7.5 * rs + ry},
-    {x = 2.5 * rs + rx, y = -12.5 * rs + ry},
-    {x = 3 * rs + rx, y = -12.5 * rs + ry},
-    {x = 3 * rs + rx, y = -9 * rs + ry})
-  local logoRed3 = addObject("logo.logo.red.3", "addPolygon", 0xff2020, .5,
-    {x = 5.5 * rs + rx, y = -7.5 * rs + ry},
-    {x = 5.5 * rs + rx, y = -12.5 * rs + ry},
-    {x = 5 * rs + rx, y = -12.5 * rs + ry},
-    {x = 5 * rs + rx, y = -9 * rs + ry})
-  local logoRed4 = addObject("logo.logo.red.4", "addPolygon", 0xff2020, .5,
-    {x = 6.5 * rs + rx, y = -12 * rs + ry},
-    {x = 6.5 * rs + rx, y = -5.75 * rs + ry},
-    {x = 8 * rs + rx, y = -5 * rs + ry},
-    {x = 11 * rs + rx, y = -5 * rs + ry},
-    {x = 11 * rs + rx, y = -12 * rs + ry})
-  local logoRed5 = addObject("logo.logo.red.5", "addPolygon", 0xff2020, .5,
-    {x = 6.5 * rs + rx, y = -4 * rs + ry},
-    {x = 6.5 * rs + rx, y = -5 * rs + ry},
-    {x = 11 * rs + rx, y = -5 * rs + ry},
-    {x = 11 * rs + rx, y = -4 * rs + ry})
-  local logoRed6 = addObject("logo.logo.red.6", "addPolygon", 0xff2020, .5,
-    {x = 6.5 * rs + rx, y = 3 * rs + ry},
-    {x = 6.5 * rs + rx, y = -2.5 * rs + ry},
-    {x = 8 * rs + rx, y = -4 * rs + ry},
-    {x = 11 * rs + rx, y = -4 * rs + ry},
-    {x = 11 * rs + rx, y = 3 * rs + ry})
-  local logoRed7 = addObject("logo.logo.red.7", "addPolygon", 0xff2020, .5,
-    {x = 4 * rs + rx, y = 3.5 * rs + ry},
-    {x = 4 * rs + rx, y = 0 * rs + ry},
-    {x = 5.5 * rs + rx, y = -1.5 * rs + ry},
-    {x = 5.5 * rs + rx, y = 3.5 * rs + ry})
-  local logoRed8 = addObject("logo.logo.red.8", "addPolygon", 0xff2020, .5,
-    {x = 4 * rs + rx, y = 3.5 * rs + ry},
-    {x = 4 * rs + rx, y = 0 * rs + ry},
-    {x = 2.5 * rs + rx, y = -1.5 * rs + ry},
-    {x = 2.5 * rs + rx, y = 3.5 * rs + ry})
-  local logoRed9 = addObject("logo.logo.red.9", "addPolygon", 0xff2020, .5,
-    {x = 1.5 * rs + rx, y = 3 * rs + ry},
-    {x = 1.5 * rs + rx, y = -2.5 * rs + ry},
-    {x = 0 * rs + rx, y = -4 * rs + ry},
-    {x = -3 * rs + rx, y = -4 * rs + ry},
-    {x = -3 * rs + rx, y = 3 * rs + ry})
-  local logoRed10 = addObject("logo.logo.red.10", "addPolygon", 0xff2020, .5,
-    {x = 1.5 * rs + rx, y = -4 * rs + ry},
-    {x = 1.5 * rs + rx, y = -5 * rs + ry},
-    {x = -3 * rs + rx, y = -5 * rs + ry},
-    {x = -3 * rs + rx, y = -4 * rs + ry})
-  local logoRed11 = addObject("logo.logo.red.11", "addPolygon", 0xff2020, .5,
-    {x = 1.5 * rs + rx, y = -12 * rs + ry},
-    {x = 1.5 * rs + rx, y = -5.75 * rs + ry},
-    {x = 0 * rs + rx, y = -5 * rs + ry},
-    {x = -3 * rs + rx, y = -5 * rs + ry},
-    {x = -3 * rs + rx, y = -12 * rs + ry})
-  local logoRed12 = addObject("logo.logo.red.12", "addPolygon", 0xff2020, .5,
-    {x = 3 * rs + rx, y = -6 * rs + ry},
-    {x = 3.5 * rs + rx, y = -7 * rs + ry},
-    {x = 4.5 * rs + rx, y = -7 * rs + ry},
-    {x = 5 * rs + rx, y = -6 * rs + ry})
+  addObject("nicks.blue.text", "addText", 217, 7, "", 0xffffff)
+  addObject("nicks.green.text", "addText", 217, 20, "", 0xffffff)
+  addObject("nicks.red.text", "addText", 217, 33, "", 0xffffff)
+  addObject("nicks.yellow.text", "addText", 217, 46, "", 0xffffff)
 end
 
 EventEngine:subscribe("init", events.priority.normal, function(handler, evt)
+  glasses.removeAll()
   drawUI()
+  events.engine:push(events.UIUpdate())
 end)
+
+EventEngine:subscribe("stop", events.priority.normal, function(handler, evt)
+  glasses.removeAll()
+end)
+
+return {
+  objects = objects
+}
--- src/glasses/update.lua
+++ src/glasses/update.lua
@@ -1,20 +1,19 @@
 local module = require("ut2-serv.modules")
 local db = module.load("db")
 local events = module.load("events")
+local surface = module.load("glasses.surface")
 
 local EventEngine = events.engine
 
 EventEngine:subscribe("uiupdate", events.priority.normal, function(handler, evt)
-  local surface = evt.surface
-
   -- 1. Update score
 
   do
     -- calculate advantages
-    local scoreB = db.teams.blue.score
-    local scoreG = db.teams.green.score
-    local scoreR = db.teams.red.score
-    local scoreY = db.teams.yellow.score
+    local scoreB = db.teams.blue.alive
+    local scoreG = db.teams.green.alive
+    local scoreR = db.teams.red.alive
+    local scoreY = db.teams.yellow.alive
     local scoreSum = scoreB + scoreG + scoreR + scoreY
 
     local adB = scoreB / scoreSum
@@ -30,10 +29,10 @@ EventEngine:subscribe("uiupdate", events.priority.normal, function(handler, evt)
     end
 
     -- set score labels
-    surface:get("score.blue.text").setText(("%02d"):format(scoreB))
-    surface:get("score.green.text").setText(("%02d"):format(scoreG))
-    surface:get("score.red.text").setText(("%02d"):format(scoreR))
-    surface:get("score.yellow.text").setText(("%02d"):format(scoreY))
+    surface.objects["score.blue.text"].setText(("%02d"):format(scoreB))
+    surface.objects["score.green.text"].setText(("%02d"):format(scoreG))
+    surface.objects["score.red.text"].setText(("%02d"):format(scoreR))
+    surface.objects["score.yellow.text"].setText(("%02d"):format(scoreY))
 
     -- set opacity
     local oB = 0.8 * adB + 0.2
@@ -41,15 +40,15 @@ EventEngine:subscribe("uiupdate", events.priority.normal, function(handler, evt)
     local oR = 0.8 * adR + 0.2
     local oY = 0.8 * adY + 0.2
 
-    surface:get("score.blue.box").setOpacity(oB)
-    surface:get("score.green.box").setOpacity(oG)
-    surface:get("score.red.box").setOpacity(oR)
-    surface:get("score.yellow.box").setOpacity(oY)
+    surface.objects["score.blue.box"].setAlpha(oB)
+    surface.objects["score.green.box"].setAlpha(oG)
+    surface.objects["score.red.box"].setAlpha(oR)
+    surface.objects["score.yellow.box"].setAlpha(oY)
 
     -- update progress bars
-    surface:get("score.red.bar").setWidth((1 - adY) * 200)
-    surface:get("score.green.bar").setWidth((1 - adY - adR) * 200)
-    surface:get("score.blue.bar").setWidth(adB * 200)
+    surface.objects["score.red.bar"].setSize(1, (1 - adY) * 200)
+    surface.objects["score.green.bar"].setSize(1, (1 - adY - adR) * 200)
+    surface.objects["score.blue.bar"].setSize(1, adB * 200)
   end
 
 
@@ -60,41 +59,30 @@ EventEngine:subscribe("uiupdate", events.priority.normal, function(handler, evt)
     local pSec = passed - pMin * 60
     pMin = ("%02d"):format(pMin)
     pSec = ("%02d"):format(pSec)
-    surface:get("time.passed.time").setText(pMin .. ":" .. pSec)
+    surface.objects["time.passed.time"].setText(pMin .. ":" .. pSec)
 
     local total = db.time
     local tMin = math.floor(total / 60)
     local tSec = total - tMin * 60
     tMin = ("%02d"):format(tMin)
     tSec = ("%02d"):format(tSec)
-    surface:get("time.total.time").setText(tMin .. ":" .. tSec)
+    surface.objects["time.total.time"].setText(tMin .. ":" .. tSec)
 
     local remain = db.remaining
     local rMin = math.floor(remain / 60)
     local rSec = remain - rMin * 60
     rMin = ("%02d"):format(rMin)
     rSec = ("%02d"):format(rSec)
-    surface:get("time.remaining.time").setText(rMin .. ":" .. rSec)
+    surface.objects["time.remaining.time"].setText(rMin .. ":" .. rSec)
 
-    surface:get("time.bar.passed").setWidth(passed / total * 200)
+    surface.objects["time.bar.passed"].setSize(1, passed / total * 200)
   end
 
   -- 3. Update nicks
   do
-    surface:get("nicks.blue.text").setText(db.teams.blue.name)
-    surface:get("nicks.green.text").setText(db.teams.green.name)
-    surface:get("nicks.red.text").setText(db.teams.red.name)
-    surface:get("nicks.yellow.text").setText(db.teams.yellow.name)
-  end
-
-  -- 4. Update admin-panel
-  do
-    if db.started then
-      surface:get("admin.startstop.box").setColor(0xff2020)
-      surface:get("admin.startstop.text").setText("Stop")
-    else
-      surface:get("admin.startstop.box").setColor(0x20afff)
-      surface:get("admin.startstop.text").setText("Start")
-    end
+    surface.objects["nicks.blue.text"].setText(db.teams.blue.name)
+    surface.objects["nicks.green.text"].setText(db.teams.green.name)
+    surface.objects["nicks.red.text"].setText(db.teams.red.name)
+    surface.objects["nicks.yellow.text"].setText(db.teams.yellow.name)
   end
 end)
